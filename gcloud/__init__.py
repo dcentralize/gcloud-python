a0a73466115b3ad32c0c0265d6c4f6dfe70fa3b5
Kees van den Broek <kvdb@d-centralize.nl>
2015-11-06 13:49:15 +0100

    fix: fix imports on GAE
    Based on: https://github.com/dhermes/test-gcloud-on-gae/commit/4457b2f846ac8f65f38e5c38fbf2258a60a67ebe

--- gcloud/__init__.py
+++ gcloud/__init__.py
@@ -14,6 +14,8 @@
 
 """GCloud API access in idiomatic Python."""
 
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('gcloud').version
+try:
+    from pkg_resources import get_distribution
+    __version__ = get_distribution('gcloud').version
+except:
+    __version__ = None
